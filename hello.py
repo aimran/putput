f4189fa0fe9c5e7f3d3684e92fc76b17cffa1427
Asif Imran <covariantmonkey@gmail.com>
2014-09-26 16:57:00 -0700

    added some dummy routines

--- hello.py
+++ hello.py
@@ -4,6 +4,14 @@ def main():
     print "Hello World!"
     print "Hello Berkeley!"
 
+    nums = range(10)
+    for n in nums:
+        print n
+
+    p = range(30,40)
+    myd = dict(zip(nums, p))
+
+
 
 if __name__ == '__main__':
     main()
