882c7972b720748adcd530fdeb461615bfcf738f
BenoitDherin <benoit.dherin@gmail.com>
2014-09-26 16:57:20 -0700

    modified substancially hello.py

--- hello.py
+++ hello.py
@@ -1,8 +1,18 @@
 #!/usr/bin/env python
 
+def what_is_a_good_name_for_a_function():
+    print "depends on how verbose you are"
+
+def my_two_cents():
+    print "write short names that tell a long story"
+
+
 def main():
-    print "Hello World!"
-    print "Hello Berkeley!"
+    print 3 + 8, "a"*10
+
+    #Hella is more politically correct than hello! 
+    print "Hella World!"
+    print "Hella Berkeley!"
 
 
 if __name__ == '__main__':
